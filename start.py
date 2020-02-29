fc7f02261c114f5b04a55b58eb374af7e8c32b4a
bnap <bt.napiorkowski@gmail.com>
2020-02-29 12:00:17 +0100

    ide do domu

---
+++ start.py
@@ -0,0 +1,30 @@
+print("Hello world!")
+
+#def main():
+#   for elem in kolekcja:
+#        costam()
+
+# łancuchy znaków
+
+liczba = "Ala"
+liczba = 0
+liczba = 0.0
+
+# PEP8
+imie = "Ala"
+print(imie)
+print(type(imie))
+print(type(5))
+print(type(4.4))
+print(type(True))
+print(type(None))
+
+imie = str("Ala")
+liczba = str(100) # "100"
+
+binarnie = "101"
+print(int(binarnie, 2))
+
+imie = imie.lower()
+# imie[] = 'a' błąd
+print(imie)
